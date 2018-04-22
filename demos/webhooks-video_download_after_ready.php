6d0c2b1157dfa2b42fea9ca5fe16a78c9d18f987
Ibnu Triyono <ibnu.triyono@gmail.com>
2018-04-22 16:09:40 +0700

    adding script for webhooks to download a file after it's ready

---
+++ demos/webhooks-video_download_after_ready.php
@@ -0,0 +1,28 @@
+<?php
+
+	$app_token = "APP_TOKEN";
+	$private_key = "PRIAVTE_KEY";
+
+	// start handling 
+
+	$webhook_data = json_decode( file_get_contents('php://input'), true );
+
+    $event_type = $webhook_data['event_type']; //it would be 'video_stream_push_success'
+
+	$video_data = $webhook_data['data']['video'];
+
+	// fwrite(fopen('php://stderr', 'w') , $event_type);
+	if($event_type == 'video_ready'){
+		// setup Ziggeo here, so it won't triggered whenever other event is called
+		require_once(dirname(__FILE__) . "/../Ziggeo.php");
+		
+		$ziggeo = new Ziggeo($app_token, $private_key);
+		
+		$video_token = $video_data['token'];
+		$file_name = $video_token.".mp4"; // create new file name based on token's
+
+		$file_content = $ziggeo->videos()->download_video($video_token) ; // get the file from server
+		file_put_contents($file_name, $file_content); // put the content to the new file
+	}
+
+?>
