0b204ea2cc8ba88c94d3e7630f05175daa71022c
Ibnu Triyono <ibnu.triyono@gmail.com>
2018-04-28 18:52:41 +0700

    fixing webhook script for deleting video after pushed

--- demos/webhooks-video_delete_after_push.php
+++ demos/webhooks-video_delete_after_push.php
@@ -1,22 +1,27 @@
 <?php
-	// setup Ziggeo and its parameter
-	require_once(dirname(__FILE__) . "/../ZiggeoPhpSdk/Ziggeo.php");
+	// add your own Application token and private key, you can get them on Ziggeo Dashboard
 
-	$ziggeo = new Ziggeo('API_TOKEN', 'PRIVATE_KEY');
+	$app_token = "f9402c3576ed13819e7e5bb4ea3a8baa";
+	$private_key = "518d2a580c4ad2cd18335eece3189a84";
 
-	//we check the webhooks responses..
+	// start handling input
 
-	// $webhook_data = json_decode( file_get_contents('php://input'), true );
-	$webhook_data = $_POST; // get POST data
+	$webhook_data = json_decode( file_get_contents('php://input'), true );
+
+    $event_type = $webhook_data['event_type']; //it would be 'video_stream_push_success'
+
+	$video_data = $webhook_data['data']['video'];
+
 	
-	if($webhook_data['event_type'] == 'video_stream_push_success'){
-		// if the webhook type is video stream push success
-		$detail_data = json_decode($webhook_data['data']); // extract video data to object
-		$video_token = $detail_data->video->token; // get pushed video token
+	if($event_type == 'video_stream_push_success'){
+		// setup Ziggeo here, so it won't triggered whenever other event is called
+		require_once(dirname(__FILE__) . "/../Ziggeo.php");
+		
+		$ziggeo = new Ziggeo($app_token, $private_key);
+		
+		$video_token = $video_data['token'];
+
 		$ziggeo->videos()->delete($video_token); // delete video by token
-		print("video ".$video_token." has been deleted");	
-	}else{
-		print("not a push success webhook");
 	}
 	
 ?>
