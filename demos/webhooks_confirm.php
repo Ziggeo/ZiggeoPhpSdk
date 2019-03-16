59653243430b5918abdaa670a7ac2e502d4fbf74
Pablo Iglesias <pablo.iglesias02@gmail.com>
2019-03-16 19:01:07 -0300

    Webhook confirm demo

---
+++ demos/webhooks_confirm.php
@@ -0,0 +1,25 @@
+<?php
+/*
+	//This script is used to create a webhook
+
+	docs: https://ziggeo.com/docs/api/webhooks/
+
+	Parameters you need to pass:
+	1. app_token
+	2. private_key
+	3. webhook_id (webhook id that's returned when using webhooks()->create())
+	4. validation_code (6-digit code sent to the webhook after creating it)
+*/
+require_once(dirname(__FILE__) . "/../Ziggeo.php");
+
+$opts = getopt("", array("app_token:", "private_key:", "webhook_id:", "validation_code:"));
+
+//We initialize the SDK
+$ziggeo = new Ziggeo($opts["app_token"], $opts["private_key"]);
+
+$ziggeo->webhooks()->confirm(array(
+	"webhook_id" => $opts["webhook_id"],
+	"validation_code" => $opts["validation_code"]
+));
+
+?>
